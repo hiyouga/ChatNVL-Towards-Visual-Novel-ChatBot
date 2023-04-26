4743ec8f990e2b89f1446da1957bb2f90f2a73d8
hiyouga <hiyouga@buaa.edu.cn>
2023-04-26 20:12:13 +0800

    update log

--- script/finetune.sh
+++ script/finetune.sh
@@ -2,19 +2,19 @@
 
 CUDA_VISIBLE_DEVICES=0 python ~/chatglm_tuning/src/finetune.py \
     --do_train \
+    --model_name_or_path ~/.cache/huggingface/hub/models--THUDM--chatglm-6b/snapshots/35ca52301fbedee885b0838da5d15b7b47faa37c \
     --dataset koikake_yui_train \
     --dataset_dir ../data \
     --finetuning_type lora \
-    --output_dir yui_230423_lora_last3_mlp_5e_5 \
+    --output_dir yui_230425_lora_full \
     --overwrite_cache \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
     --save_steps 1000 \
-    --learning_rate 5e-5 \
-    --weight_decay 1e-4 \
-    --num_train_epochs 5.0 \
-    --lora_target 25.mlp.dense_h_to_4h,25.mlp.dense_4h_to_h,26.mlp.dense_h_to_4h,26.mlp.dense_4h_to_h,27.mlp.dense_h_to_4h,27.mlp.dense_4h_to_h \
+    --learning_rate 1e-4 \
+    --num_train_epochs 3.0 \
+    --lora_target 25.attention.query_key_value,26.attention.query_key_value,27.attention.query_key_value \
     --plot_loss \
     --fp16
